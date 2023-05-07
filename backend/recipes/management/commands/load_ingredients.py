c374436571de1540dee2a6a11ee488a27c27e8fd
Магомед Мулаев <howardbreton@mail.ru>
2023-05-07 14:29:53 +0300

    created base command for load_data

---
+++ backend/recipes/management/commands/load_ingredients.py
@@ -0,0 +1,28 @@
+import csv
+import os
+
+from django.core.management.base import BaseCommand
+from recipes.models import Ingredient
+from api_foodgram import settings
+
+
+def ingredient_create(row):
+    Ingredient.objects.get_or_create(
+        name=row[0],
+        measurement_unit=row[1]
+    )
+
+
+class Command(BaseCommand):
+    help = "Load ingredients to DB"
+
+    def handle(self, *args, **options):
+        path = os.path.join(settings.BASE_DIR, 'ingredients.csv')
+        with open(path, 'r', encoding='utf-8') as file:
+            row_count = sum(1 for row in file)
+        with open(path, 'r', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                ingredient_create(row)
+        self.stdout.write("Загрузка ингредиентов завершена")
